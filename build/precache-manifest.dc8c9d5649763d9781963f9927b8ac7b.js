b566c95bbf1c2eb6c986a66b704ff8bc2957cf67
Fatih Topçu <FatihTopcu97@gmail.com>
2022-06-26 14:53:13 +0300

    page build

---
+++ build/precache-manifest.dc8c9d5649763d9781963f9927b8ac7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26741bc1c67f65777890fcfda965c47f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21719006c421f9f2430e",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fd1cbc0f3c3b7420b298",
+    "url": "./static/css/main.2b6db94d.chunk.css"
+  },
+  {
+    "revision": "21719006c421f9f2430e",
+    "url": "./static/js/2.70a7c459.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.70a7c459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1cbc0f3c3b7420b298",
+    "url": "./static/js/main.dba5ea5b.chunk.js"
+  },
+  {
+    "revision": "67d4f361b93adbbab73d",
+    "url": "./static/js/runtime-main.9cc45873.js"
+  }
+]);
