e1918dd53201f8e65669b9d265d0c082d9965f35
Fatih-Topcu <fatihtopcu97@gmail.com>
2023-12-22 04:44:04 +0100

    Updates

---
+++ precache-manifest.dc23b194b2781f72ad730fe53bceb04f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31e0e63a2ad4323421bb8ddf2a4cac0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3c6a55d1078e461b318",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5b70ed903ed995ea0daa",
+    "url": "./static/css/main.0bbc6e3e.chunk.css"
+  },
+  {
+    "revision": "b3c6a55d1078e461b318",
+    "url": "./static/js/2.c1478fcb.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.c1478fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b70ed903ed995ea0daa",
+    "url": "./static/js/main.7abdd8c7.chunk.js"
+  },
+  {
+    "revision": "67d4f361b93adbbab73d",
+    "url": "./static/js/runtime-main.9cc45873.js"
+  }
+]);
